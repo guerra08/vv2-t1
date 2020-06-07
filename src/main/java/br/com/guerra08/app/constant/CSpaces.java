99da2f1272cbdab60b5263346b8cf916b8558d05
Bruno Guerra <gguerrabruno@outlook.com>
2020-06-06 23:15:44 -0300

    Adding modal

--- src/main/java/br/com/guerra08/app/constant/CSpaces.java
+++ src/main/java/br/com/guerra08/app/constant/CSpaces.java
@@ -2,7 +2,7 @@ package br.com.guerra08.app.constant;
 
 public class CSpaces {
 
-    public static final double PRICE_PER_SQUARE = 30.00;
-    public static final double PRICE_PER_SEAT = 10.00;
+    public static double PRICE_PER_SQUARE = 30.00;
+    public static double PRICE_PER_SEAT = 10.00;
 
 }
