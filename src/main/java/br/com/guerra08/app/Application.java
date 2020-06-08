cdd72f1a506268dd63482849c6a7ff91a3a96249
Bruno Guerra <gguerrabruno@outlook.com>
2020-06-08 14:00:39 -0300

    Fix filter

--- src/main/java/br/com/guerra08/app/Application.java
+++ src/main/java/br/com/guerra08/app/Application.java
@@ -9,6 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 
 @SpringBootApplication
@@ -19,17 +20,21 @@ public class Application {
     }
 
     @Bean
-    public ApplicationRunner initializer(CollaboratorRepository colRepo, ResourceRepository resourceRepository) {
+    public ApplicationRunner initializer(CollaboratorRepository colRepo, ResourceRepository resourceRepository, ReservationRepository resRepo) {
+
+        Collaborator colToReserve = new Collaborator("12345", "João da Silva", "jsilva@outlook.com");
+        Resource resoToReserve = new Furniture("Mesa", 15, "Mobília");
+
         return args ->  {
             colRepo.saveAll(Arrays.asList(
-                    new Collaborator("12345", "João da Silva", "jsilva@outlook.com"),
+                    colToReserve,
                     new Collaborator("54321", "Marcos Júnior", "junior.m@gmail.com"),
                     new Collaborator("11223", "Felipe Castro", "castrofelipe@yahoo.com"),
                     new Collaborator("90998", "Pedro Silva", "psilva@hotmail.com"),
                     new Collaborator("87866", "Luis Volpato", "volpluis@outlook.com")
             ));
             resourceRepository.saveAll(Arrays.asList(
-                new Furniture("Mesa", 15, "Mobília"),
+                resoToReserve,
                 new Furniture("Quadro", 17, "Mobília"),
                 new Furniture("Armário de cadernos", 20, "Armário"),
                 new MobileEquip("Motorola G5", 25, "Smartphone"),
@@ -40,6 +45,7 @@ public class Application {
                 new Spaces("Sala 20", 10, 30, "Sala"),
                 new Spaces("Área externa", 20, 100, "Área")
             ));
+            resRepo.save(new Reservation(resoToReserve, colToReserve, LocalDate.of(2020, 3, 17), LocalDate.of(2020, 3, 21)));
         };
     }
 
